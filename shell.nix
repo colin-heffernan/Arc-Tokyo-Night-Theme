15c9dcb83ba42e1a770b086b44c162d7c330aac3
Colin Heffernan <colinpheffernan@gmail.com>
2022-09-09 16:24:27 -0400

    Write a shell.nix to make installation easier

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {  } }:
+pkgs.mkShell {
+	name = "Arc Tokyo Night Build Shell";
+	buildInputs = with pkgs; [
+		meson
+		ninja
+		glib.dev
+		sassc
+	];
+
+	shellHook = ''
+		meson setup --prefix=$HOME/.local -Dthemes=gtk2,gtk3,gtk4 -Dvariants=dark build/
+		meson install -C build/
+	'';
+}
